43b0012e41d57414be1f10faf42c99ee5dec7e5b
Gayathri Ramakrishnan <gayathri.t.ramakrishnan@gmail.com>
2026-01-15 10:21:41 -0600

    create agent

--- my_first_agent/agent.py
+++ my_first_agent/agent.py
@@ -1,8 +1,13 @@
 from google.adk.agents.llm_agent import Agent
 
+def greeting_tool()-> str:
+    """Returns a warm friendly welcome message"""
+    return "Hello from your specialized greeting tool! Welcome."
+
 root_agent = Agent(
     model='gemini-2.5-flash',
     name='root_agent',
     description='A helpful assistant for user questions.',
     instruction='Answer user questions to the best of your knowledge',
 )
+

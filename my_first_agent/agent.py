7caadacf06685d8f991f282ba19c782671d396c8
Gayathri Ramakrishnan <gayathri.t.ramakrishnan@gmail.com>
2026-03-06 19:14:41 -0600

    feat: add input and output schema

--- my_first_agent/agent.py
+++ my_first_agent/agent.py
@@ -6,6 +6,15 @@ from pydantic import BaseModel, Field
 #     """Returns a warm friendly welcome message"""
 #     return "Hello from your specialized greeting tool! Welcome."
 
+class GreetingRequest(BaseModel):
+    """Input schema for specifying the language of the greeting."""
+    language: str = Field(description="The language to greet the user in.")
+
+class GreetingResponse(BaseModel):
+    """Output schema for the greeting message."""
+    greeting_message: str = Field(description="The final, formatted greeting message.")
+    
+
 root_agent = Agent(
     model='gemini-2.5-flash',
     name='root_agent',
@@ -14,8 +23,12 @@ root_agent = Agent(
     generate_content_config=types.GenerateContentConfig(
         temperature=0.2,
         max_output_tokens=250,
+        # input_schema=GreetingRequest,
+        # output_schema=GreetingResponse,
+        # output_key="final_greeting",
+        # include_contents = 'none', # It will be a fresh conversation. History will not be included in the input.
         safety_settings=[types.SafetySetting(
-            category=types.SafetySetting.Category.HARM_CATEGORY_DANGEROUS_CONTENT,
+            category=types.HarmCategory.HARM_CATEGORY_HATE_SPEECH,
             threshold=types.HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
         )]
     )
